53b2f2e6d346fb32390f41514e02c0ef0ebdd613
Frank Broy <f.r.f.broy@student.tudelft.nl>
2021-07-06 14:22:41 +0200

    Add joining servers from invite codes and scanning the newly joined servers

--- bin/feeder.py
+++ bin/feeder.py
@@ -2,7 +2,9 @@ import discum
 import json
 import hashlib
 import base64
-import gzip
+import time
+import sys
+import os
 
 t = open("etc/token.txt", "r").read()
 client = discum.Client(token=t, log={"console":False, "file":False})
@@ -12,27 +14,36 @@ def start(resp):
     if resp.event.ready_supplemental: #ready_supplemental is sent after ready
         user = client.gateway.session.user
         print("Logged in as {}#{}".format(user['username'], user['discriminator']))
-        scanServers()
 
-def scanServers():
-    servers = client.getGuilds().json()
-    # print("The user is in the following servers: ")
-    # print(json.dumps(servers, indent=4))
-    for server in servers:
-        # All possible channel types: GUILD_TEXT, DM, GUILD_VOICE, GROUP_DM, GUILD_CATEGORY, GUILD_NEWS, GUILD_STORE, GUILD_NEWS_THREAD, GUILD_PUBLIC_THREAD, GUILD_PRIVATE_THREAD, GUILD_STAGE_VOICE
-        # For now we only consider the text channels
-        channel_types = ['guild_text', 'dm', 'group_dm', 'guild_news', 'guild_store', 'guild_news_thread', 'guild_public_thread', 'guild_private_thread']
-        channels = client.gateway.findVisibleChannels(server['id'], channel_types)
-        # print("The channels in server '" + i['name'] + "' are: ")
-        # print(json.dumps(channels, indent=4))
-        for channel in channels:
-            messages = client.getMessages(channel, 5).json()
-            # print("The messages in channel " + j + " are: ")
-            # print(json.dumps(messages, indent=4))
-            for message in messages:
-                info = createJson(message, server, channel)
-                print("The formatted metadata of the message is:")
-                print(json.dumps(info, indent=4))
+        # Scan the servers the user is already on
+        servers = client.getGuilds().json()
+        for server in servers:
+            scanServer(server)
+        
+        print("Sleeping for 5 seconds to avoid rate limits")
+        time.sleep(5)
+        # Once we scanned all the servers the user is already on, we join the ones from server-invite-codes.txt and search those as well
+        joinServers()
+        print("Done! Exiting now...")
+        os._exit(0)
+
+def scanServer(server):
+    # All possible channel types: 
+    # guild_text, dm, guild_voice, group_dm, guild_category, guild_news, guild_store, guild_news_thread, guild_public_thread, guild_private_thread, guild_stage_voice
+    # For now we only consider the text channels
+    channel_types = ['guild_text', 'dm', 'group_dm', 'guild_news', 'guild_store', 'guild_news_thread', 'guild_public_thread', 'guild_private_thread']
+    channels = client.gateway.findVisibleChannels(server['id'], channel_types)
+    
+    # print("The channels in server '" + i['name'] + "' are: ")
+    # print(json.dumps(channels, indent=4))
+    for channel in channels:
+        messages = client.getMessages(channel, 5).json()
+        # print("The messages in channel " + j + " are: ")
+        # print(json.dumps(messages, indent=4))
+        for message in messages:
+            info = createJson(message, server, channel)
+            print("The formatted metadata of the message is:")
+            print(json.dumps(info, indent=4, sort_keys=True))
 
 def createJson(message, server, channel):
     info = {}
@@ -43,7 +54,6 @@ def createJson(message, server, channel):
     content_bytes = message['content'].encode('utf-8')
     info['message:content-sha256'] = hashlib.sha256(content_bytes).hexdigest()
     info['message:content'] = base64.b64encode(content_bytes).decode('utf-8')
-    
     #info['message:content'] = message['content']
 
     info['channel:id'] = message['channel_id']
@@ -51,6 +61,7 @@ def createJson(message, server, channel):
     info['sender:profile'] = message['author']['username'] + "#" + message['author']['discriminator']
     info['server:id'] = server['id']
     info['server:name'] = server['name']
+
     info['attachments'] = []
     for attachment in message['attachments']:
         a = {}
@@ -94,27 +105,34 @@ def createJson(message, server, channel):
     info['embedded-objects'] = []
     for embedded in message['embeds']:
         e = {}
-        # All the fields that exist: title, type, url, description, timestamp, color, footer, image, thumbnail, video, provider, author, fields
+        # All the fields that exist: 
+        # title, type, url, description, timestamp, color, footer, image, thumbnail, video, provider, author, fields
         fields = ['title', 'type', 'url', 'description']
         for field in fields:
             if field in embedded:
                 e[field] = embedded[field]
         info['embedded-objects'].append(e)
 
-    # Get the message that is referenced and recursively analyse the thread
     if 'message_reference' in message:
         info['referenced-message'] = {}
         info['referenced-message']['guild-id'] = message['message_reference']['guild_id']
         info['referenced-message']['channel-id'] = message['message_reference']['channel_id']
         info['referenced-message']['message-id'] = message['message_reference']['message_id']
         info['referenced-message']['url'] = "https://discord.com/channels/" + message['message_reference']['guild_id'] + "/" + message['message_reference']['channel_id'] + "/" + message['message_reference']['message_id']
-        # Call the message analyser method recursively
+        # TODO: Call the message analyser method recursively
     return info
 
-def joinServer():
-    code = input("Invitation code: ")
-    print("Trying to join the server now...")
-    client.joinGuild(code)
-    print("Joined the server successfully!")
+def joinServers():
+    codes = open("etc/server-invite-codes.txt", "r")
+    for code in codes:
+        print("Invite code: " + code)
+        print("Trying to join the server now...")
+        # The waiting time can be reduced, but the lower the time waited between joining servers, the higher the risk to get banned
+        client.joinGuild(code, wait=10)
+        print("Joined the server successfully!")
+        print("Scanning the newly joined server...")
+        server = client.getInfoFromInviteCode(code).json()['guild']
+        scanServer(server)
+        print("Scan successful")
 
 client.gateway.run(auto_reconnect=True)
